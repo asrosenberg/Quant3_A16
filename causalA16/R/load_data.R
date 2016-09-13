1dc5af5466fbc621b05240559cac35eb68526b27
asrosenberg <rosenberg.1108@osu.edu>
2016-09-13 12:21:18 -0400

    added descriptive appendix and finished

--- causalA16/R/load_data.R
+++ causalA16/R/load_data.R
@@ -7,9 +7,7 @@
 #' @export
 load_dataset <- function(dataset)
 {
-  fn <- system.file("extdata",
-    paste0(dataset,".csv"),
-    package = "causalA16")
+  fn <- system.file("extdata", paste0(dataset,".csv"), package = "causalA16")
   DATA <- read.csv(fn)
 }
 
